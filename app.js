9d70b1e028606d1965a71884c921ef4840efd942
Alexandru Rosianu <me@aluxian.com>
2017-02-04 13:30:26 +0000

    Add /plantrip

--- app.js
+++ app.js
@@ -46,46 +46,19 @@ bot.beginDialogAction('hello', '/hello', { matches: /^(hello|hi|what'?s? up|good
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/hello', [
-  function (session) {
-      var card = new builder.HeroCard(session)
-          .title("Hello mate, long time no talk.")
-          .text("Where do you want to go?")
-          .images([
-               builder.CardImage.create(session, "https://www.google.co.uk/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiNlKLZwPbRAhVMnRQKHVBHBFUQjRwIBQ&url=https%3A%2F%2Fwww.theodysseyonline.com%2Fwanderlust-the-desire-travel&psig=AFQjCNF7zrAr16mUOqKVeGFW6wgMFCdBVQ&ust=1486299890533843")
-          ]);
-      var msg = new builder.Message(session).attachments([card]);
-      session.send(msg);
-      session.send("");
-      session.beginDialog('/travel');
-  },
-  function (session, results) {
-      // Display menu
-      session.beginDialog('/travelmenu');
-  },
-  function (session, results) {
-      // Always say goodbye
-      session.send("Ok... Bye bye!");
-  }
-]);
-
 bot.dialog('/', [
   function (session) {
-      // Send a greeting and show help.
-      var card = new builder.HeroCard(session)
-          .title("Travound")
-          .text("Find the best getaways for your budget");
-      var msg = new builder.Message(session).attachments([card]);
-      session.send(msg);
-      session.send("Hi! I can find awesome trips for you. Where do you want to go?");
-      session.beginDialog('/');
+    var card = new builder.HeroCard(session)
+      .title("Travound")
+      .text("Find the best getaways for your budget");
+    var msg = new builder.Message(session).attachments([card]);
+    session.send(msg);
+    session.beginDialog('/plantrip');
   },
-  function (session, results) {
-      // Display menu
-      session.beginDialog('/menu');
-  },
-  function (session, results) {
-      // Always say goodbye
-      session.send("Ok... See you later!");
+]);
+
+bot.dialog('/plantrip', [
+  function (session) {
+    session.send("Hi! I can find awesome trips for you. Where do you want to go?");
   }
 ]);
