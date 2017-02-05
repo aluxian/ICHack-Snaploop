62ffba1048ffaa2aef0bc257457374827231530b
Alexandru Rosianu <me@aluxian.com>
2017-02-05 10:48:00 +0000

    Remove comma

--- app.js
+++ app.js
@@ -303,7 +303,7 @@ bot.dialog('/guess', [
           // start snapping dialog for the chosen one
           bot.send(
             new builder.Message(session).text('You\'re the chosen one! 🍀'),
-            function(err) { if (err) { console.error(err); } },
+            function(err) { if (err) { console.error(err); } }
           );
           bot.beginDialog(chosenAddress, '/snap');
 
